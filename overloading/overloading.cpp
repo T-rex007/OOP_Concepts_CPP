4b280cc929f9baa860f5a806b30a216fba95d994
T-rex007 <shaqc777@yahoo.com>
2019-09-11 23:58:02 -0400

    -----

--- overloading/overloading.cpp
+++ overloading/overloading.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "overloading.hpp"
 
 int main(){
+
     
+    int a, b,c; 
+    Compare com;
+    std::cout<<"Input Numbers:"<<std::endl;
+    std::cin>>a;
+    std::cin>>b;
+    std::cin>>c;
+
+    //Compare c;
+    // testing find smaller
+    std::cout<<"Answer: "<< com.findSmaller(a,b,c);
+
+
+
+
+
     return 0;
 }
