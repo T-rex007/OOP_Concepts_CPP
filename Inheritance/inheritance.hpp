742790c88cc347f1a859bc34d702a78550c3a9fc
T-rex007 <shaqc777@yahoo.com>
2019-09-18 22:20:22 -0400

    ----

---
+++ Inheritance/inheritance.hpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+
+//#################################################### Base Class "Shape" ############################################################
+class Shape {
+    protected:
+        float width;
+        float height;
+    public:
+        void setWidth(float val);
+        void setHeight(float val);
+        float getWidth();
+        float getHeight();
+        Shape(int a, int b);
+};
+
+Shape::Shape(int a, int b){
+    width = a;
+    height = b;
+}
+
+//Mutators
+
+void Shape::setWidth(float val){
+    width = val ;
+}
+void Shape::setHeight(float val ){
+    height = val;
+}
+
+// Accessors
+
+float Shape::getWidth(){
+    return width;
+}
+float Shape::getHeight(){
+    return height;
+}
+//#######################################################################################################################################
+//#########################################################SubClasses####################################################################
+
+
+class Square: protected Shape{
+    public:
+        float getArea();
+
+};
+
+Square::
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
